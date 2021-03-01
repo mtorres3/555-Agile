af760c84d3c1da5f57067e4b5e34508b91d13200
ebuczek525 <ebuczek@stevens.edu>
2021-03-01 17:31:22 -0500

    Eb sprint1 (#3)
    * EB Sprint 1 Done
    
    * Reverted changes that I made to the gedcom files
    
    * Removed my pytest file, merged newest changes within main py file

--- GEDCOM Reader/Family.py
+++ GEDCOM Reader/Family.py
@@ -1,12 +1,14 @@
 class Family:
-    def __init__(self, ID = "", Married = "Date Not Found", Divorced = "NA", Husband_ID = "", Husband_Name = "", Wife_ID = "", Wife_Name = "", Children = None):
+    def __init__(self, ID = "", Married = "Date Not Found", Divorced = "NA", Husband_ID = "", Husband_Name = "", Husband_death_date = "NA" ,Wife_ID = "", Wife_Name = "", Wife_death_date = "NA", Children = None):
         self.ID = ID # String
         self.married = Married # String
         self.divorced = Divorced # String
         self.husband_id = Husband_ID # String
         self.husband_name = Husband_Name # String
+        self.husband_death_date = Husband_death_date # String
         self.wife_id = Wife_ID # String
         self.wife_name = Wife_Name # String
+        self.wife_death_date = Wife_death_date # String
         if Children is None:
             Children = []
         self.children = Children # List
