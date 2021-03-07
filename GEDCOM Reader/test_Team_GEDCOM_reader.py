93424f5ec90f6cd0f9675532c46f480417321e55
Jon Cucci <62709104+joncucci@users.noreply.github.com>
2021-03-07 13:15:29 -0500

    Update test_Team_GEDCOM_reader.py

--- GEDCOM Reader/test_Team_GEDCOM_reader.py
+++ GEDCOM Reader/test_Team_GEDCOM_reader.py
@@ -39,37 +39,32 @@ class TestGEDCOMReader(unittest.TestCase):
         
     # Checks to see if individual ID matches the GEDCOM file
     def individual_ID(self):
-        self.assertEqual(fil.individuals[0].ID, "I1")
-        self.assertEqual(fil.individuals[0].name, "Jonathan Cucci")
+        self.assertEqual(individuals[0].ID, "I1")
 
     # Checks to see if fam ID matches GEDCOM file, as well as the husband and wife IDs and Names.
     def family_ID(self):
-        self.assertEqual(fil.families[0].ID, "F1")
-        self.assertEqual(fil.families[0].husband_id, "I1")
-        self.assertEqual(fil.families[0].husband_name, "Jonathan Cucci")
-        self.assertEqual(fil.families[0].wife_id, "I2")
-        self.assertEqual(fil.families[0].wife_name, "Valery Dzevel")
+        self.assertEqual(families[0].ID, "F1")
+        self.assertEqual(families[0].husband_id, "I1")
+        self.assertEqual(families[0].wife_id, "I2")
 
     # Testing to see if Children match to the correct family
     def family_children(self):
-        self.assertEqual(fil.families[1].ID, "F2")
-        self.assertEqual(fil.families[1].children, ["I1", "I5"])
-        self.assertEqual(fil.families[1].husband_name, "Michael Cucci")
-        self.assertEqual(fil.families[1].wife_name, "Katherine Isacson")
+        self.assertEqual(families[1].ID, "F2")
+        self.assertEqual(families[1].children, ["I1", "I5"])
 
     # Testing to ensure Unique Individual IDs
     def unique_individual_IDs(self):
         unique_IDs = []
-        for num in range(len(fil.individuals)):
-            unique_IDs.append(fil.individuals[num].ID)
+        for num in range(len(individuals)):
+            unique_IDs.append(individuals[num].ID)
         for ID in range(len(unique_IDs)):
             self.assertEqual(unique_IDs.count(unique_IDs[ID]), 1)
 
     # Testing to ensure Unique Family IDs
     def unique_family_IDs(self):
         unique_Fam_IDs = []
-        for num in range(len(fil.families)):
-            unique_Fam_IDs.append(fil.families[num].ID)
+        for num in range(len(families)):
+            unique_Fam_IDs.append(families[num].ID)
         for ID in range(len(unique_Fam_IDs)):
             self.assertEqual(unique_Fam_IDs.count(unique_Fam_IDs[ID]), 1)
 
