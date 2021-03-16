37d2dcc8ce6827ebee1c4442569b07c0c2028394
Jon Cucci <62709104+joncucci@users.noreply.github.com>
2021-03-16 12:53:11 -0400

    SQLite Database

--- GEDCOM Reader/Team_GEDCOM_reader.py
+++ GEDCOM Reader/Team_GEDCOM_reader.py
@@ -322,29 +322,3 @@ with open('Letizia_GEDTEST.ged.txt') as fam:
     conn.commit()
     print("successful")
 
-'''
-        # if there is a tag in the first index value that can be found
-        # in the TAG list corresponding its level val and manipulate
-        #  the string to add Y
-        if(newLine[1] in tags[int(newLine[0])]):
-            newLine[1] = "|" + newLine[1] + "|" + "Y|"
-        # Else if the list after slicing is larger than 2 pieces, and the
-        # index 2 element is in the TAGS list with a corresponding level value,
-        # change the 1st index to the tag and put the id after
-        elif(len(newLine) > 2 and newLine[2] in tags[int(newLine[0])]):
-            temp = newLine[2]
-            newLine[2] = newLine[1]
-            newLine[1] = "|" + temp + "|" + "Y|"
-        # Else, manipulate index 1 to add N. It is not a compatable TAG.
-        else:
-             newLine[1] = "|" + newLine[1] + "|" + "N|"
-        # Input / Output formatting
-        line = "--> " + line
-        printLine = "<--"
-        # Concatenation
-        for bit in newLine:
-            printLine = printLine + " " + bit
-        # Annnnnnnnd Print
-        print(line[:-1])
-        print(printLine + "\n")
-'''
